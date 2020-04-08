68b544bec8ff6ea8e9d592bb626412b94974a5e2
Jessica <jessica.manent@gmail.com>
2020-04-08 21:10:58 +0200

    feat: add folder exercice01

---
+++ exercice01/main.ts
@@ -0,0 +1,14 @@
+class Book {
+  title: string;
+  price: number;
+
+  constructor(title: string, price: number) {
+    this.title = title
+    this.price = price
+  }
+}
+
+let toup: Book = new Book("toup", 12); // une instance de la classe Book
+let nep: Book = new Book("nep", 12); // une seconde instance de la classe Book
+
+console.log(toup.title, toup.price);
